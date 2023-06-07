b022beb276e65773a5ec8a7f647bba9e582038a1
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-06-07 12:42:16 -0300

    Update UrnaResource.java

--- src/main/java/urnaEletronica/project/resources/UrnaResource.java
+++ src/main/java/urnaEletronica/project/resources/UrnaResource.java
@@ -1,8 +1,8 @@
 package urnaEletronica.project.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import urnaEletronica.project.domain.entities.Eleitor;
 import urnaEletronica.project.services.UrnaService;
@@ -16,6 +16,7 @@ public class UrnaResource {
     private UrnaService urnaService;
 
     @PutMapping(value = "/number/{number}/titulo/{titulo}")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Eleitor> vote(@PathVariable Integer number,@PathVariable Long titulo){
         Eleitor obj;
         obj = urnaService.vote(number, titulo);
